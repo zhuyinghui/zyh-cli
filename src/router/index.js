791f292a47aa6693daae64af1e77bb9d7956ddde
zhuyinghui <404993465@qq.com>
2020-07-05 01:54:35 +0800

    build:路由自动化搞定

--- src/router/index.js
+++ src/router/index.js
@@ -3,74 +3,40 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-// 布局内的页面
-const frameIn = [
+const frameOut = [
   {
-    path: '',
-    redirect: '/home',
-    component: () => import('../views/layout/index.vue'),
-    children: [
-      {
-        path: '/home',
-        name: 'home',
-        component: () => import('../views/layout/home/index.vue')
-      },
-      {
-        path: '/about',
-        name: 'about',
-        component: () => import('../views/layout/about/index.vue')
-      }
-    ]
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/login/index.vue')
   }
 ]
 
-// 布局外的页面
-const frameOut = [
-  // {
-  //   path: '/login',
-  //   name: 'login',
-  //   component: () => import('../views/login/index.vue')
-  // }
-]
-
 const routers = require.context('../views/layout', true, /\/index.vue$/)
-const effectivePath = []
 const autoLoadRoutes = []
 for (const item of routers.keys()) {
   const path = item.replace(/\.\//, '/').replace(/\/index.vue/, '')
   const name = item.replace(/\.\//, '').replace(/index.vue/, '').replace(/\//g, '') || 'layout'
-  effectivePath.push({
-    path,
-    name,
-    // component: item.replace(/\.\//, '../views/layout/'),
-    file: routers(item).default
-  })
-  // autoLoadRoutes.push(Vue.component(name, routers(item).default))
+  if (path) {
+    autoLoadRoutes.push({
+      path,
+      name,
+      component: routers(item).default
+    })
+  }
 }
-// routers.keys().forEach(name => {
-//   const componentName = name.replace(/\.\//, '').replace(/index.vue/, '').replace(/\//g, '') || 'layout'
-//   const componentDefault = routers(name)
-// Vue.component(componentName, componentDefault.default)
-// })
-console.log('vue文件 ', effectivePath)
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    ...frameIn,
-    ...frameOut,
-    ...autoLoadRoutes
+    {
+      path: '',
+      redirect: '/home',
+      component: () => import('../views/layout/index.vue'),
+      children: autoLoadRoutes
+    },
+    ...frameOut
   ]
 })
-console.log('最终路由', [
-  ...frameIn,
-  ...frameOut,
-  ...autoLoadRoutes
-])
-
-// router.afterEach(to => {
-//   console.log('路由', to)
-// })
 
 export default router
